403e810dd95d1483fabacde235d41871b3d0e031
Nishant Shrivastava <nishant7sri@gmail.com>
2017-10-14 14:08:46 +0530

    Removed spaces

--- Calculator/calculator.c
+++ Calculator/calculator.c
@@ -1,17 +1,12 @@
 # include <stdio.h>
-
 int main() {
-
     char op;
     double n1,n2;
-
     printf("Enter an operator (+, -, *,/): ");
     scanf("%c",&op);
-
     printf("Enter two operands: ");
     scanf("%lf%lf",&n1,&n2);
-
-    switch(op)
+     switch(op)
     {
         case '+':
             printf("%.2f + %.2f = %.2f\n",n1, n2, n1 + n2);
